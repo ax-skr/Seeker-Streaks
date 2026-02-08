a2103f4e1a1ae5ef809529bc8a16810ca63f70cc
ax-skr <axh.skr@gmail.com>
2026-02-08 14:06:52 +0000

    lots of changes

--- app/manifest.webmanifest/route.ts
+++ app/manifest.webmanifest/route.ts
@@ -4,20 +4,36 @@ export async function GET() {
   const manifest = {
     name: "Seeker Streaks",
     short_name: "Streaks",
+    id: "/",
     start_url: "/",
     scope: "/",
     display: "standalone",
     background_color: "#020617",
     theme_color: "#020617",
     icons: [
-      { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
-      { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/icon-maskable-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
     ],
   };
 
-  return new NextResponse(JSON.stringify(manifest), {
+  return NextResponse.json(manifest, {
     headers: {
-      "Content-Type": "application/manifest+json",
+      // Bubblewrap is happy with application/manifest+json
+      "Content-Type": "application/manifest+json; charset=utf-8",
       "Cache-Control": "no-store",
     },
   });
