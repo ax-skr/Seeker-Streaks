fc2bb5c8923cc3f2e5717f77d1d6ec8d320d973e
ax-skr <axh.skr@gmail.com>
2026-05-30 16:27:11 +0100

    Update payments.ts

--- lib/payments.ts
+++ lib/payments.ts
@@ -1,7 +1,7 @@
 export const SKR_MINT = "SKRbvo6Gf7GondiT3BbTfuRDPqLWei4j2Qy2NPGZhW3";
 export const TREASURY_WALLET = "Bd92q41ZZ12s5HorfajEfSpYZFTxWWod46vhtwChemT";
 
-export const SKR_PER_DAY = 500;
+export const SKR_PER_DAY = 100;
 
 // DB limit (keep name for DB logic)
 export const MAX_RESCUE_DAYS = 4;
